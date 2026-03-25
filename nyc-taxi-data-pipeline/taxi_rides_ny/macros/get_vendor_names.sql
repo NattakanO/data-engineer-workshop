fad7b8fc4d5a19446bc1fc4ed281af809aea334e
Nattakan <nattakan.o1998@gmail.com>
2026-03-25 21:16:30 +1100

    add macro functions

---
+++ nyc-taxi-data-pipeline/taxi_rides_ny/macros/get_vendor_names.sql
@@ -0,0 +1,8 @@
+{# Returns the name of a vendor based on their ID #}
+{% macro get_vendor_names(vendor_id) %}
+    case
+            when {{vendor_id}} = 1 then 'Creative Mobile Technologies, LLC'
+            when {{vendor_id}} = 2 then 'VeriFone Inc.'
+            when {{vendor_id}} = 4 then 'Unknown Vendor'
+        end as vendor_name
+{% endmacro %}
