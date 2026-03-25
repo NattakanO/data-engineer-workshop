87dd7102d7ead91e6c66292a575591e8f852e387
Nattakan <nattakan.o1998@gmail.com>
2026-03-25 21:16:47 +1100

    add dbt models

---
+++ nyc-taxi-data-pipeline/taxi_rides_ny/models/marts/dim_vendors.sql
@@ -0,0 +1,15 @@
+-- Dimension table for taxi technology vendors
+-- Small static dimension defining vendor codes and their company names
+
+with trips_unioned as (
+    select * from {{ ref('int_trips_unioned') }}
+),
+
+vendors as (
+    select distinct
+        vendor_id,
+        {{get_vendor_names('vendor_id')}}
+    from trips_unioned
+)
+
+select * from vendors
